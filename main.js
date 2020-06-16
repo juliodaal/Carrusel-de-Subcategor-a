d79fdcd1d9f3bb4a9eeac09eab5426ed5ad8acca
Julio Daal Sousa <32969470+juliodaal@users.noreply.github.com>
2020-06-16 20:23:06 +0100

    V1

---
+++ main.js
@@ -0,0 +1,23 @@
+const buttonPrev = document.getElementById('button-prev');
+const buttonNext = document.getElementById('button-next');
+const track = document.getElementById('track');
+const slickList = document.getElementById('slick-list');
+const slick = document.querySelectorAll('.slick');
+
+const slickWidth = slick[0].offsetWidth;
+
+buttonPrev.onclick = () => Move(1);
+buttonNext.onclick = () => Move(2);
+
+function Move(value) {
+    const trackWidth = track.offsetWidth;
+    const listWidth = slickList.offsetWidth;
+
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+
+    if(leftPosition < (trackWidth - listWidth) && value == 2) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    } else if(leftPosition > 0 && value == 1) {
+        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+    }
+}
