d02e58a1d1dcf3108381d4a91f677ef53222d890
Julio Daal Sousa <32969470+juliodaal@users.noreply.github.com>
2021-01-17 17:52:45 +0000

    Multiples Carruseles

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-const buttonPrev = document.getElementById('button-prev');
-const buttonNext = document.getElementById('button-next');
-const track = document.getElementById('track');
-const slickList = document.getElementById('slick-list');
-const slick = document.querySelectorAll('.slick');
+function App() {}
 
-const slickWidth = slick[0].offsetWidth;
+window.onload = function (event) {
+    var app = new App();
+    window.app = app;
+};
 
-buttonPrev.onclick = () => Move(1);
-buttonNext.onclick = () => Move(2);
+App.prototype.processingButton = function(event) {
+    const btn = event.currentTarget;
+    const slickList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const slick = track.querySelectorAll('.slick');
 
-function Move(value) {
+    const slickWidth = slick[0].offsetWidth;
+    
     const trackWidth = track.offsetWidth;
     const listWidth = slickList.offsetWidth;
 
-    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+    track.style.left == ""  ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
 
-    if(leftPosition < (trackWidth - listWidth) && value == 2) {
-        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
-    } else if(leftPosition > 0 && value == 1) {
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition,slickWidth,track) : nextAction(leftPosition,trackWidth,listWidth,slickWidth,track)
+}
+
+let prevAction = (leftPosition,slickWidth,track) => {
+    if(leftPosition > 0) {
+        console.log("entro 2")
         track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+    }
+}
+
+let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
+    if(leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
     }
 }
